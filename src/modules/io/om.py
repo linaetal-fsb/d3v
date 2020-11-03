abbb6ea32b19c94356914e10d3d436ed7f92aa4b
SilvioAs <Silvio_Achilles@web.de>
2020-11-03 19:53:38 +0100

    Added option to import polygons

--- src/modules/io/om.py
+++ src/modules/io/om.py
@@ -11,6 +11,7 @@ class OpenMeshImporter(IOHandler):
         g = Geometry()
         try:
             m = om.read_trimesh(fileName)
+            # m = om.read_polymesh(fileName)
         except:
             print("File not supported for read with openmesh")
             return
